44b01093dec8b38c957cc66c2523ddd99504ebbb
pasharyan <pasharyan50@gmail.com>
2026-04-20 10:29:23 +0700

    menambahkan main

--- IntroductionOOP.cpp
+++ IntroductionOOP.cpp
@@ -12,4 +12,14 @@ void printdata()
     cout << "NIM : " << nim << endl;
     cout << "Nama : " << nama << endl;
     cout << "Nilai : " << nilai << endl;
+}; // batas class
+
+int main()
+{
+    Mahasiswa mhs;
+    mhs.nim = 2022;
+    mhs.nama = "Abra";
+    mhs.nilai = 90.5;
+
+    mhs.printdata();
 }
